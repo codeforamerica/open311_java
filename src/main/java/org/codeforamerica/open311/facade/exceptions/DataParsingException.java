523c85169f65041d6e6c2feeab07592c81ddb608
Santiago Munín <burning1@gmail.com>
2013-06-15 13:31:14 +0200

    Added a data parsing exception

---
+++ src/main/java/org/codeforamerica/open311/facade/exceptions/DataParsingException.java
@@ -0,0 +1,13 @@
+package org.codeforamerica.open311.facade.exceptions;
+
+/**
+ * Represents a problem in the parsing of any string.
+ * 
+ * @author Santiago Munín <santimunin@gmail.com>
+ * 
+ */
+public class DataParsingException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
