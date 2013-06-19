07c631f6557eaef97222d7782ed5d29e5e39c966
Santiago Munín <burning1@gmail.com>
2013-06-19 20:30:21 +0200

    Created an URLBuilder with the responsability of building URLs

--- src/main/java/org/codeforamerica/open311/internals/network/URLBuilder.java
+++ src/main/java/org/codeforamerica/open311/internals/network/URLBuilder.java
@@ -2,8 +2,39 @@ package org.codeforamerica.open311.internals.network;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class URLBuilder {
+
+	/**
+	 * List of the possible optional arguments of the POST Service Request
+	 * operation.
+	 */
+	private static final String[] POST_SERVICE_REQUEST_OPTIONAL_ARGUMENTS = {
+			"api_key", "lat", "long", "address_id", "address_string", "email",
+			"device_id", "account_id", "first_name", "last_name", "phone",
+			"description", "media_url" };
+	/**
+	 * List of the possible optional arguments of the GET Service Requests
+	 * operation.
+	 */
+	private static final String[] GET_SERVICE_REQUESTS_OPTIONAL_ARGUMENTS = {
+			"service_request_id", "service_code", "start_date", "end_date",
+			"status" };
+	/**
+	 * List of the possible optional arguments of the GET Service Request
+	 * operation.
+	 */
+	private static final String[] GET_SERVICE_REQUEST_OPTIONAL_ARGUMENTS = {
+			"service_request_id", "status", "status_notes", "service_name",
+			"service_code", "description", "agency_responsible",
+			"service_notice", "request_datetime", "updated_datetime",
+			"expected_datetime", "address", "address_id", "zipcode", "lat",
+			"long", "media_url" };
+
 	private static final String GET_SERVICE_LIST = "services";
 	private static final String GET_SERVICE_DEFINITION = "services";
 	private static final String POST_SERVICE_REQUEST = "requests";
@@ -17,33 +48,196 @@ public class URLBuilder {
 		this.format = format;
 	}
 
-	public URL buildGetServiceListUrl() throws MalformedURLException {
-		return new URL(baseUrl + "/" + GET_SERVICE_LIST + "." + format);
-	}
-
-	public URL buildGetServiceDefinitionUrl(String serviceCode)
+	/**
+	 * Builds a GET Service List URL.
+	 * 
+	 * @param jurisdictionId
+	 *            Required if the same endpoint holds different places.
+	 * @return An URL ready to be accessed.
+	 * @throws MalformedURLException
+	 *             If one of the parts of the url (endpoint's base url,
+	 *             format...) is not correct.
+	 */
+	public URL buildGetServiceListUrl(String jurisdictionId)
 			throws MalformedURLException {
-		return new URL(baseUrl + "/" + GET_SERVICE_DEFINITION + "/"
-				+ serviceCode + "." + format);
+		String url = baseUrl + "/" + GET_SERVICE_LIST + "." + format;
+		return new URL(addJurisdictionId(url, jurisdictionId));
 	}
 
-	public URL buildPostServiceRequestUrl() throws MalformedURLException {
-		return new URL(baseUrl + "/" + POST_SERVICE_REQUEST + "." + format);
+	/**
+	 * Builds a GET Service Definition URL.
+	 * 
+	 * @param jurisdictionId
+	 *            Required if the same endpoint holds different places.
+	 * @param serviceCode
+	 *            Code of the service to find the definition.
+	 * @return An URL ready to be accessed.
+	 * @throws MalformedURLException
+	 *             If one of the parts of the url (endpoint's base url,
+	 *             format...) is not correct.
+	 */
+	public URL buildGetServiceDefinitionUrl(String jurisdictionId,
+			String serviceCode) throws MalformedURLException {
+		String url = baseUrl + "/" + GET_SERVICE_DEFINITION + "/" + serviceCode
+				+ "." + format;
+		return new URL(addJurisdictionId(url, jurisdictionId));
 	}
 
-	public URL buildGetServiceRequestIdFromATokenUrl(String token)
+	/**
+	 * Builds a POST Service Request URL.
+	 * 
+	 * @param apiKey
+	 *            Key required for the operation.
+	 * @param jurisdictionId
+	 *            Required if the same endpoint holds different places.
+	 * @param arguments
+	 *            Pairs (key, value) of optional arguments
+	 * @return An URL ready to be accessed.
+	 * @throws MalformedURLException
+	 *             If one of the parts of the url (endpoint's base url,
+	 *             format...) is not correct.
+	 */
+	public URL buildPostServiceRequestUrl(String apiKey, String jurisdictionId,
+			Map<String, String> arguments) throws MalformedURLException {
+		Set<String> validArguments = new HashSet<String>(
+				Arrays.asList(POST_SERVICE_REQUEST_OPTIONAL_ARGUMENTS));
+		validatearguments(arguments, validArguments);
+		if (jurisdictionId.length() > 0) {
+			arguments.put("jurisdiction_id", jurisdictionId);
+		}
+		arguments.put("api_key", apiKey);
+		return buildUrl(baseUrl + "/" + POST_SERVICE_REQUEST + "." + format,
+				arguments);
+	}
+
+	/**
+	 * Builds a GET Service Request URL.
+	 * 
+	 * @param jurisdictionId
+	 *            Required if the same endpoint holds different places.
+	 * @param token
+	 *            Token associated to a service request.
+	 * @return An URL ready to be accessed.
+	 * @throws MalformedURLException
+	 *             If one of the parts of the url (endpoint's base url,
+	 *             format...) is not correct.
+	 */
+	public URL buildGetServiceRequestIdFromATokenUrl(String jurisdictionId,
+			String token) throws MalformedURLException {
+		String url = baseUrl + "/" + GET_SERVICE_REQUEST_FROM_A_TOKEN + "/"
+				+ token + "." + format;
+		return new URL(addJurisdictionId(url, jurisdictionId));
+	}
+
+	/**
+	 * Builds a GET Service Request URL.
+	 * 
+	 * @param jurisdictionId
+	 *            Required if the same endpoint holds different places.
+	 * @param arguments
+	 *            Pairs (key, value) of optional arguments
+	 * @return An URL ready to be accessed.
+	 * @throws MalformedURLException
+	 *             If one of the parts of the url (endpoint's base url,
+	 *             format...) is not correct.
+	 */
+	public URL buildGetServiceRequests(String jurisdictionId,
+			Map<String, String> arguments) throws MalformedURLException {
+		Set<String> validArguments = new HashSet<String>(
+				Arrays.asList(GET_SERVICE_REQUESTS_OPTIONAL_ARGUMENTS));
+		validatearguments(arguments, validArguments);
+		if (jurisdictionId.length() > 0) {
+			arguments.put("jurisdiction_id", jurisdictionId);
+		}
+		return buildUrl(baseUrl + "/" + GET_SERVICE_REQUESTS + "." + format,
+				arguments);
+	}
+
+	/**
+	 * Builds a GET Service Request URL.
+	 * 
+	 * @param jurisdictionId
+	 *            Required if the same endpoint holds different places.
+	 * @param serviceId
+	 *            Id of the service.
+	 * @param arguments
+	 *            Pairs (key, value) of optional arguments
+	 * @return An URL ready to be accessed.
+	 * @throws MalformedURLException
+	 *             If one of the parts of the url (endpoint's base url,
+	 *             format...) is not correct.
+	 */
+	public URL buildGetServiceRequest(String jurisdictionId, String serviceId,
+			Map<String, String> arguments) throws MalformedURLException {
+		Set<String> validArguments = new HashSet<String>(
+				Arrays.asList(GET_SERVICE_REQUEST_OPTIONAL_ARGUMENTS));
+		validatearguments(arguments, validArguments);
+		if (jurisdictionId.length() > 0) {
+			arguments.put("jurisdiction_id", jurisdictionId);
+		}
+		return buildUrl(baseUrl + "/" + GET_SERVICE_REQUESTS + "/" + serviceId
+				+ "." + format, arguments);
+	}
+
+	/**
+	 * Checks if any given argument is not valid.
+	 * 
+	 * @param givenArguments
+	 *            Arguments given to the operation.
+	 * @param allowedArguments
+	 *            Set of all the valid arguments of an operation.
+	 * @throws MalformedURLException
+	 *             If any of the given arguments is not valid.
+	 */
+	private void validatearguments(Map<String, String> givenArguments,
+			Set<String> allowedArguments) throws MalformedURLException {
+		for (String key : givenArguments.keySet()) {
+			if (!allowedArguments.contains(key)) {
+				throw new MalformedURLException("Invalid optional argument: "
+						+ key);
+			}
+		}
+	}
+
+	/**
+	 * Builds an URL from a base plus a list of arguments.
+	 * 
+	 * @param base
+	 *            Host address.
+	 * @param arguments
+	 *            List of pairs (key, value).
+	 * @return a URL built from the given data.
+	 * @throws MalformedURLException
+	 *             If there was any problem building the URL.
+	 */
+	private URL buildUrl(String base, Map<String, String> arguments)
 			throws MalformedURLException {
-		return new URL(baseUrl + "/" + GET_SERVICE_REQUEST_FROM_A_TOKEN + "/"
-				+ token + "." + format);
+		StringBuilder builder = new StringBuilder();
+		for (String key : arguments.keySet()) {
+			builder.append(key + "=" + arguments.get(key) + "&");
+		}
+		String argumentsPart = builder.toString();
+		if (argumentsPart.length() > 0) {
+			base += "?";
+			argumentsPart = argumentsPart.substring(0,
+					argumentsPart.length() - 1);
+		}
+		return new URL(base + argumentsPart);
 	}
 
-	public URL buildGetServicesRequests() throws MalformedURLException {
-		return new URL(baseUrl + "/" + GET_SERVICE_REQUESTS + "." + format);
-	}
-
-	public URL buildGetServiceRequest(String serviceId)
-			throws MalformedURLException {
-		return new URL(baseUrl + "/" + GET_SERVICE_REQUESTS + "/" + serviceId
-				+ "." + format);
+	/**
+	 * Adds a jurisdictionId to an url.
+	 * 
+	 * @param url
+	 *            Base url.
+	 * @param jurisdictionId
+	 *            Given jurisdiction id.
+	 * @return The new url.
+	 */
+	private String addJurisdictionId(String url, String jurisdictionId) {
+		if (jurisdictionId.length() > 0) {
+			return url += "?jurisdiction_id=" + jurisdictionId;
+		}
+		return url;
 	}
 }
--- src/test/java/org/codeforamerica/open311/internals/network/URLBuilderTest.java
+++ src/test/java/org/codeforamerica/open311/internals/network/URLBuilderTest.java
@@ -1,9 +1,13 @@
 package org.codeforamerica.open311.internals.network;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,8 +19,11 @@ import org.junit.Test;
  * 
  */
 public class URLBuilderTest {
+
 	private static final String BASE_URL = "https://api.city.gov/dev/v2";
 	private static final String FORMAT = "xml";
+	private static final String JURISDICTION_ID = "city.gov";
+	private URLBuilder builder = new URLBuilder(BASE_URL, FORMAT);;
 
 	@BeforeClass
 	public static void testInitialization() {
@@ -29,20 +36,68 @@ public class URLBuilderTest {
 	}
 
 	@Test
-	public void urlBuilderTest() throws MalformedURLException {
-		URLBuilder builder = new URLBuilder(BASE_URL, FORMAT);
-		assertEquals(builder.buildGetServiceListUrl().toString(),
-				"https://api.city.gov/dev/v2/services.xml");
-		assertEquals(builder.buildGetServiceDefinitionUrl("033").toString(),
-				"https://api.city.gov/dev/v2/services/033.xml");
-		assertEquals(builder.buildPostServiceRequestUrl().toString(),
-				"https://api.city.gov/dev/v2/requests.xml");
-		assertEquals(builder.buildGetServicesRequests().toString(), BASE_URL
-				+ "/" + "requests" + "." + FORMAT);
-		assertEquals(builder.buildGetServiceRequestIdFromATokenUrl("123456")
-				.toString(), "https://api.city.gov/dev/v2/tokens/123456.xml");
-		assertEquals(builder.buildGetServiceRequest("123456").toString(),
-				"https://api.city.gov/dev/v2/requests/123456.xml");
+	public void urlBuilderWithoutOptionalArgumentsTest()
+			throws MalformedURLException {
+		assertEquals(
+				builder.buildGetServiceListUrl(JURISDICTION_ID).toString(),
+				"https://api.city.gov/dev/v2/services.xml?jurisdiction_id="
+						+ JURISDICTION_ID);
+		assertEquals(
+				builder.buildGetServiceDefinitionUrl(JURISDICTION_ID, "033")
+						.toString(),
+				"https://api.city.gov/dev/v2/services/033.xml?jurisdiction_id="
+						+ JURISDICTION_ID);
+		assertEquals(
+				builder.buildGetServiceRequestIdFromATokenUrl(JURISDICTION_ID,
+						"123456").toString(),
+				"https://api.city.gov/dev/v2/tokens/123456.xml?jurisdiction_id="
+						+ JURISDICTION_ID);
+	}
+
+	@Test
+	public void urlBuilderWithOptionalArgumentsTest()
+			throws MalformedURLException {
+		Map<String, String> arguments = new HashMap<String, String>();
+		arguments.put("lat", "8.12");
+		arguments.put("long", "4.12");
+		arguments.put("account_id", "1");
+
+		String url = builder.buildPostServiceRequestUrl("key", JURISDICTION_ID,
+				arguments).toString();
+		assertTrue(url.contains("https://api.city.gov/dev/v2/requests.xml?"));
+		assertTrue(url.contains("lat=8.12"));
+		assertTrue(url.contains("long=4.12"));
+		assertTrue(url.contains("account_id=1"));
+		assertTrue(url.contains("jurisdiction_id=city.gov"));
+		assertTrue(url.contains("api_key=key"));
+		assertEquals(StringUtils.countMatches(url, "&"), 4);
+
+		arguments = new HashMap<String, String>();
+		arguments.put("start_date", "2010-05-24T00:00:00Z");
+		arguments.put("end_date", "2010-06-24T00:00:00Z");
+		arguments.put("status", "open");
+		url = builder.buildGetServiceRequests(JURISDICTION_ID, arguments)
+				.toString();
+		assertTrue(url.contains("https://api.city.gov/dev/v2/requests.xml?"));
+		assertTrue(url.contains("start_date=2010-05-24T00:00:00Z"));
+		assertTrue(url.contains("end_date=2010-06-24T00:00:00Z"));
+		assertTrue(url.contains("status=open"));
+		assertTrue(url.contains("jurisdiction_id=city.gov"));
+		assertEquals(StringUtils.countMatches(url, "&"), 3);
+
+		arguments = new HashMap<String, String>();
+		arguments.put("service_request_id", "2");
+		arguments.put("status_notes", "test");
+		arguments.put("status", "open");
+		url = builder.buildGetServiceRequest(JURISDICTION_ID, "123456",
+				arguments).toString();
+		assertTrue(url
+				.contains("https://api.city.gov/dev/v2/requests/123456.xml?"));
+		assertTrue(url.contains("service_request_id=2"));
+		assertTrue(url.contains("status_notes=test"));
+		assertTrue(url.contains("status=open"));
+		assertTrue(url.contains("jurisdiction_id=city.gov"));
+		assertEquals(StringUtils.countMatches(url, "&"), 3);
 
 	}
 }
