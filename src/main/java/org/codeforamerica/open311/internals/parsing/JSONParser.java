5ae0a7131adf48d324089e92ce741cabe9a6e8b0
Santiago Munín <burning1@gmail.com>
2013-07-10 15:47:25 +0200

    If the ZipCode isn't given the JSONParser no longer crashes

--- src/main/java/org/codeforamerica/open311/internals/parsing/JSONParser.java
+++ src/main/java/org/codeforamerica/open311/internals/parsing/JSONParser.java
@@ -149,7 +149,7 @@ public class JSONParser extends AbstractParser {
 						serviceRequest, EXPECTED_DATETIME_TAG));
 				String address = getString(serviceRequest, ADDRESS_TAG);
 				long addressId = getLong(serviceRequest, ADDRESS_ID_TAG);
-				int zipCode = serviceRequest.getInt(ZIPCODE_TAG);
+				int zipCode = (int) getLong(serviceRequest, ZIPCODE_TAG);
 				float latitude = (float) serviceRequest.getDouble(LATITUDE_TAG);
 				float longitude = (float) serviceRequest
 						.getDouble(LONGITUDE_TAG);
